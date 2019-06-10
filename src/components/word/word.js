73384b36526417cee2f09795630f1fdc0d03dab8
Mark Wyatt <markprwyatt@gmail.com>
2019-06-10 15:33:51 +0100

    Added random word

--- src/components/word/word.js
+++ src/components/word/word.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import randomWords from 'random-words';
+import './word.css';
+
+const randomWord = randomWords(1) + ''
 
 const word = () => {
     return (
-        <div>
-            This is the word.
+        <div className="word">
+           <div className="letter">{'_ '.repeat(randomWord.length)}</div> 
         </div>
     )
 }
